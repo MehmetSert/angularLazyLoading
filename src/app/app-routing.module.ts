9e671d071e1831492bcb003ca4119345ba10dd9e
mhmtsrt11@gmail.com <44hm3ts3rt>
2019-05-24 11:42:36 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: './components/home/home.module#HomeModule', pathMatch: 'full' },
+  { path: 'about', loadChildren: './components/about/about.module#AboutModule' },
+  { path: 'account', loadChildren: './components/account/account.module#AccountModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
